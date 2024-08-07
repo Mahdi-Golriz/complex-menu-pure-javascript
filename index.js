3f1ab3c4433332b2ced42762400df23fa003557d
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-08-07 09:13:13 +0200

    second commit

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ let Menu = function () {
   let _config = {
     index: 0,
     items: Array.from(document.querySelector("ul.items").children),
+    displayDiv: document.querySelector("div.display"),
   };
 
-  function _selectItem() {}
+  function _displayImages() {
+    _config.items.forEach((item) =>
+      item.addEventListener("click", () => {
+        const figures = item.querySelectorAll("figure");
+
+        _config.displayDiv.innerHTML = "";
+        figures.forEach((item) => {
+          const clonedImage = item.cloneNode(true);
+          _config.displayDiv.appendChild(clonedImage);
+        });
+        _adjustPictureHeight();
+      })
+    );
+  }
 
   function _adjustPictureHeight() {
     var containerHeight = 586;
-    var numImages = document.querySelector("div.pictures").children.length;
+    var numImages = document.querySelector("div.display").children.length;
+
     var ItemsHeight = containerHeight / (Math.floor((numImages - 1) / 3) + 1);
     document.documentElement.style.setProperty("--height", `${ItemsHeight}px`);
+    console.log(ItemsHeight);
   }
 
   function _adjustArrowTop() {
@@ -23,8 +39,8 @@ let Menu = function () {
     });
   }
 
+  _displayImages();
   _adjustArrowTop();
-  _adjustPictureHeight();
 };
 
 window.Menu = Menu;
