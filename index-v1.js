03b9814b640aef56e6f24c9bfd704ed82ffa3662
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-08-09 15:23:06 +0200

    implementing image display

---
+++ index-v1.js
@@ -0,0 +1,69 @@
+let Menu = function () {
+  let _config = {
+    index: 0,
+    items: Array.from(document.querySelector("ul.items").children),
+    displayDiv: document.querySelector("div.display"),
+  };
+
+  function _displayImages() {
+    _config.items.forEach((item, i) =>
+      item.addEventListener("click", () => {
+        if (i === _config.index) return;
+
+        _removePreviousImages();
+
+        setTimeout(() => {
+          _showNewImages(item);
+          _adjustPictureHeight();
+        }, 400);
+
+        _config.items.forEach((i) => {
+          i.classList.contains("active") && i.classList.remove("active");
+        });
+        item.classList.add("active");
+
+        _config.index = i;
+      })
+    );
+  }
+
+  function _removePreviousImages() {
+    Array.from(_config.displayDiv.children).forEach((figure) => {
+      figure.classList.remove("scale-enter");
+      figure.classList.add("scale-exit");
+    });
+  }
+
+  function _showNewImages(item) {
+    const figures = item.querySelectorAll("figure");
+    _config.displayDiv.innerHTML = "";
+    figures.forEach((item) => {
+      const clonedImage = item.cloneNode(true);
+      _config.displayDiv.appendChild(clonedImage);
+      clonedImage.classList.add("scale-enter");
+    });
+  }
+
+  function _adjustPictureHeight() {
+    var containerHeight = 586;
+    var numImages = document.querySelector("div.display").children.length;
+
+    var ItemsHeight = containerHeight / (Math.floor((numImages - 1) / 3) + 1);
+    document.documentElement.style.setProperty("--height", `${ItemsHeight}px`);
+    console.log(ItemsHeight);
+  }
+
+  function _adjustArrowLevel() {
+    _config.items.forEach((item, i) => {
+      item.addEventListener("click", () => {
+        document.documentElement.style.setProperty("--top", `${i * 13 + 2}rem`);
+      });
+    });
+  }
+
+  _displayImages();
+  _adjustArrowLevel();
+};
+
+window.Menu = Menu;
+Menu();
--- index.js
+++ index.js
@@ -1,68 +1,97 @@
 let Menu = function () {
   let _config = {
-    index: 0,
-    items: Array.from(document.querySelector("ul.items").children),
-    displayDiv: document.querySelector("div.display"),
+    activeIndex: 0,
+    items: Array.from(
+      document.querySelector(".first-level__container").children
+    ),
+    menuItems: Array.from(document.querySelector("ul.items").children),
+    isAnimating: false,
   };
 
   function _displayImages() {
-    _config.items.forEach((item, i) =>
+    _config.menuItems.forEach((item, i) =>
       item.addEventListener("click", () => {
-        if (i === _config.index) return;
+        if (i === _config.activeIndex || _config.isAnimating) return;
 
-        _removePreviousImages();
+        _config.isAnimating = true;
+
+        Array.from(_config.items[_config.activeIndex].children).forEach(
+          (figure, i) => {
+            setTimeout(() => _removePreviousImages(figure), i * 30);
+          }
+        );
 
         setTimeout(() => {
-          _showNewImages(item);
-          _adjustPictureHeight();
-        }, 400);
+          _changeActiveIndex(i);
+          _config.isAnimating = false;
+        }, _config.items.length * 30 + 1000);
 
-        _config.items.forEach((i) => {
-          i.classList.contains("active") && i.classList.remove("active");
+        Array.from(_config.items[i].children).forEach((figure, i) => {
+          setTimeout(() => {
+            _showNewImages(figure);
+          }, _config.items.length * 30 + 1000 + i * 30);
         });
-        item.classList.add("active");
-
-        _config.index = i;
       })
     );
   }
 
-  function _removePreviousImages() {
-    Array.from(_config.displayDiv.children).forEach((figure) => {
-      figure.classList.remove("scale-enter");
-      figure.classList.add("scale-exit");
-    });
+  function _removePreviousImages(element, opacity = 1, scale = 1) {
+    if (opacity > 0 || scale > 0.7) {
+      opacity -= 1 / 30;
+      scale -= 0.3 / 30;
+
+      element.style.opacity = opacity;
+      element.style.transform = `scale(${scale})`;
+
+      requestAnimationFrame(() =>
+        _removePreviousImages(element, opacity, scale)
+      );
+    }
   }
 
-  function _showNewImages(item) {
-    const figures = item.querySelectorAll("figure");
-    _config.displayDiv.innerHTML = "";
-    figures.forEach((item) => {
-      const clonedImage = item.cloneNode(true);
-      _config.displayDiv.appendChild(clonedImage);
-      clonedImage.classList.add("scale-enter");
-    });
+  function _showNewImages(element, opacity = 0, scale = 0.7) {
+    if (opacity < 1 || scale < 1) {
+      opacity += 1 / 30;
+      scale += 0.3 / 30;
+
+      element.style.opacity = opacity;
+      element.style.transform = `scale(${scale})`;
+
+      requestAnimationFrame(() => _showNewImages(element, opacity, scale));
+    }
+  }
+
+  function _changeActiveIndex(i) {
+    _config.items[_config.activeIndex].classList.remove("active");
+
+    _config.activeIndex = i;
+    _config.items[_config.activeIndex].classList.add("active");
+    _adjustPictureHeight();
   }
 
   function _adjustPictureHeight() {
-    var containerHeight = 586;
-    var numImages = document.querySelector("div.display").children.length;
+    var firstContainerHeight =
+      document.querySelector("div.items.active").offsetHeight;
+    var numImages = document.querySelector("div.items.active").children.length;
 
-    var ItemsHeight = containerHeight / (Math.floor((numImages - 1) / 3) + 1);
+    var ItemsHeight =
+      firstContainerHeight / (Math.floor((numImages - 1) / 3) + 1);
     document.documentElement.style.setProperty("--height", `${ItemsHeight}px`);
-    console.log(ItemsHeight);
   }
 
-  function _adjustArrowHeight() {
-    _config.items.forEach((item, i) => {
-      item.addEventListener("click", () => {
-        document.documentElement.style.setProperty("--top", `${i * 13 + 2}rem`);
-      });
+  function _addEventListeners() {
+    window.addEventListener("resize", _adjustPictureHeight);
+    document.addEventListener("DOMContentLoaded", () => {
+      Array.from(_config.items[_config.activeIndex].children).forEach(
+        (figure, i) => {
+          setTimeout(() => _showNewImages(figure), i * 60);
+        }
+      );
     });
   }
 
+  _addEventListeners();
   _displayImages();
-  _adjustArrowHeight();
 };
 
 window.Menu = Menu;
